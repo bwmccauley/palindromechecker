fa8f156aec21acc858350b95530fd51c6b83ab59
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2025-08-10 10:15:37 -0400

    inital commit

---
+++ JavaTestClass.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class JavaTestClass {
+    public static void mai (String [] arguments) {
+        int number;
+        System.out.println("enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        number = sc.nextInt();
+    }
+    
+}
